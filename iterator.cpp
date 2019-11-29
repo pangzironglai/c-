38a3d83ec0a0aa46335ac173e614aa75d19c5e9a
pangzironglai <48469725+pangzironglai@users.noreply.github.com>
2019-11-29 14:52:19 +0800

    Update iterator.cpp

--- iterator.cpp
+++ iterator.cpp
@@ -1,1 +1,17 @@
+#include<string>
+
+/*
+使用iterator 输出 字符串中字符
+*/
+int main()
+{
+  string str;
+  cin >> str;
+  string :: iterator ptr = str.begin();
+  while(ptr != str.end())
+  {
+      cout << *(ptr++)<< "\n"; 
+  }
+  return 0;
+}
 
