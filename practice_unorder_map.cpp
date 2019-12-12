34decf8f0e38fceab6ef9d5d9077503e92a42604
pangzironglai <48469725+pangzironglai@users.noreply.github.com>
2019-12-12 14:37:01 +0800

    Add files via upload

---
+++ practice_unorder_map.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+#include <algorithm> 
+using namespace std;
+
+class Solution
+{
+private:
+	vector<int> path;
+	vector<vector<int>> res;
+	vector<int>candidates;
+
+public:
+	void DFS(int start, int target)
+	{
+		if (target == 0)
+		{
+			res.push_back(path);
+			return;
+		}
+		for (int i = start; i < candidates.size() && target - candidates[i] >= 0; i++)
+		{
+			path.push_back(candidates[i]);
+			DFS(i, target - candidates[i]);
+			path.pop_back();
+		}
+	}
+
+	vector<vector<int>>combinationSum(vector<int>&candicates, int target)
+	{
+		sort(candicates.begin(), candicates.end());
+		this->candidates = candicates;
+		DFS(0, target);
+		return res;
+
+
+	}
+
+};
+
+int main()
+{
+	Solution Sol;
+	vector<int>candicates = { 2,3,6,7 };
+	int targer = 7;
+	vector<vector<int>>res = Sol.combinationSum(candicates, targer);
+	cout << " ";
+}
